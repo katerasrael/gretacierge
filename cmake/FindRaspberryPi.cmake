d8abd13a1bf451e531ec65bea813b1d21bab9d98
katerasrael <asbaer@asrael.franken.de>
2019-01-21 13:47:20 +0100

    notice the Raspberry Pi 3

--- cmake/FindRaspberryPi.cmake
+++ cmake/FindRaspberryPi.cmake
@@ -8,8 +8,8 @@ function (CHECK_IS_RASPBERRY_PI IS_RPI MODEL)
         # Serial        : 00000000649760c2
 
         # Find CPU hardware.
-        string(REGEX MATCH "Hardware[ \t]+:[ \t]+(BCM270[89])" RES "${CPUINFO}")
-        string(REGEX REPLACE "Hardware[ \t]+:[ \t]+(BCM270[89])" "\\1" RES "${RES}")
+        string(REGEX MATCH "Hardware[ \t]+:[ \t]+(BCM2[78][03][589])" RES "${CPUINFO}")
+        string(REGEX REPLACE "Hardware[ \t]+:[ \t]+(BCM2[78][03][589])" "\\1" RES "${RES}")
         #message("Hardware: ${RES}")
 
         if ("${RES}" STREQUAL "BCM2708")
@@ -18,6 +18,9 @@ function (CHECK_IS_RASPBERRY_PI IS_RPI MODEL)
         elseif("${RES}" STREQUAL "BCM2709")
             set(${IS_RPI} 1 PARENT_SCOPE)
             set(${MODEL} "Raspberry Pi 2" PARENT_SCOPE)
+        elseif("${RES}" STREQUAL "BCM2835")
+            set(${IS_RPI} 1 PARENT_SCOPE)
+            set(${MODEL} "Raspberry Pi 3" PARENT_SCOPE)
         endif()
     else()
         set(${IS_RPI} 0 PARENT_SCOPE)
