8a0b77bf326c420a3d252ac21eef496fd7b2e02a
katerasrael <asbaer@asrael.franken.de>
2019-02-12 15:49:34 +0100

    direction sort out

--- extra/catcierge-telegram.py
+++ extra/catcierge-telegram.py
@@ -89,12 +89,16 @@ def create_row(imgs, offsets, gap, fixed_width=0, caption=None, caption_offset=(
 
 def compose_img(img_paths=None, match_json=None, gap=5, horizontal_gap=5, description=None, caption="Catcierge", state="undefined", direction="undefined"):
 
-    if state == "Lockout":
-        background_color =  "#ff0000"   #red
-    elif state == "Keep open":
-        background_color =  "#ADFF2F"   #green
+    if direction == "In":
+        if state == "Lockout":
+            background_color =  "#ff0000"   #red
+        elif state == "Keep open":
+            background_color =  "#ADFF2F"   #green
+        else:
+            background_color =  "#8A968E"   #original grey
     else:
         background_color =  "#8A968E"   #original grey
+
 
     img = Image(width=600, height=1124, background=Color(background_color))
         
@@ -134,7 +138,7 @@ def compose_img(img_paths=None, match_json=None, gap=5, horizontal_gap=5, descri
     # TODO 
     if description:
         desc_font = Font(path="%s/source-code-pro/SourceCodePro-Medium.otf" % args.fonts, size=24)
-        font_width = int(desc_font.size * 0.7)
+        font_width = int(desc_font.size * 0.8)
         
         if description == "Skipped prey detection when going out":
             text = direction + ' - Skipped prey detection' # https://stackoverflow.com/questions/2872512/python-truncate-a-long-string#
@@ -263,7 +267,7 @@ def create_matches(catcierge_json, output_file, args):
         img = compose_img(img_paths=img_paths,
                 gap=5,
                 description=match["description"],
-                caption="Match %d" % i, state=catcierge_json["state"], direction=match["direction"])
+                caption="Match %d" % i, state=catcierge_json["state"], catcierge_json["match_group_direction"])
 
         total_width += img.width
         total_height = max(total_height, img.height)
