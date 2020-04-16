354ac5696b4221f40a4868b5313defcaccd39c64
katerasrael <asbaer@asrael.franken.de>
2020-04-16 16:41:15 +0200

    fix how to determine which Raspberry (1/2/3/4) were working on

--- cmake/FindRaspberryPi.cmake
+++ cmake/FindRaspberryPi.cmake
@@ -1,28 +1,32 @@
 function (CHECK_IS_RASPBERRY_PI IS_RPI MODEL)
     if (UNIX AND NOT APPLE)
-        file(READ /proc/cpuinfo CPUINFO)
-
-        # Example of what we're looking for:
-        # Hardware      : BCM2709
-        # Revision      : a01041
-        # Serial        : 00000000649760c2
-
-        # Find CPU hardware.
-        string(REGEX MATCH "Hardware[ \t]+:[ \t]+(BCM2[78][03][589])" RES "${CPUINFO}")
-        string(REGEX REPLACE "Hardware[ \t]+:[ \t]+(BCM2[78][03][589])" "\\1" RES "${RES}")
-        #message("Hardware: ${RES}")
-
-        if ("${RES}" STREQUAL "BCM2708")
+        # https://www.raspberrypi.org/documentation/hardware/raspberrypi/revision-codes/README.md
+        
+        # Copied from
+        # https://github.com/juj/fbcp-ili9341/blob/master/CMakeLists.txt
+        # Detect if current hardware is Raspberry Pi Zero/Zero W, and enable targeting Zero with -DSINGLE_CORE_BOARD=ON if so.
+		#
+		execute_process(COMMAND cat /proc/cpuinfo OUTPUT_VARIABLE CPU_INFO)
+		STRING(REGEX MATCH "Revision[\t ]*:[\t ]*([0-9a-f]+)" BOARD_REVISION ${CPU_INFO})
+		set(BOARD_REVISION "${CMAKE_MATCH_1}")
+		message(STATUS "Board revision: ${CMAKE_MATCH_1}")
+		
+		# http://ozzmaker.com/check-raspberry-software-hardware-version-command-line/
+		if (BOARD_REVISION MATCHES "(0002)|(0003)|(0004)|(0005)|(0006)|(0007)|(0008)|(0009)" OR BOARD_REVISION MATCHES "(000d)|(000e)|(000f)|(0010)|(0011)|(0012)" OR BOARD_REVISION MATCHES "(900092)|(900093)|(9000c1)")
+			message(STATUS "Detected this Pi to be one of: Pi A, A+, B rev. 1, B rev. 2, B+, CM1, Zero or Zero W, with single hardware core and ARMv6Z instruction set CPU.")
             set(${IS_RPI} 1 PARENT_SCOPE)
             set(${MODEL} "Raspberry Pi" PARENT_SCOPE)
-        elseif("${RES}" STREQUAL "BCM2709")
+        elseif(BOARD_REVISION MATCHES "(a01041)|(a21041)")
+			message(STATUS "Detected this board to be a Pi 2 Model B < rev 1.2 with ARMv7-A instruction set CPU.")
             set(${IS_RPI} 1 PARENT_SCOPE)
             set(${MODEL} "Raspberry Pi 2" PARENT_SCOPE)
-        elseif("${RES}" STREQUAL "BCM2835")
+		elseif(BOARD_REVISION MATCHES "(a02082)|(a22082)|(a020d3)|(9020e0)|(a03111)|(b03111)|(c03111)")
+			message(STATUS "Detected this Pi to be one of: Pi 2B rev. 1.2, 3B, 3B+, 3A+, CM3, CM3 lite or 4B(1GB,2GB,4GB RAM), with 4 hardware cores and ARMv8-A instruction set CPU.")
             set(${IS_RPI} 1 PARENT_SCOPE)
-            set(${MODEL} "Raspberry Pi 3" PARENT_SCOPE)
-        endif()
-    else()
-        set(${IS_RPI} 0 PARENT_SCOPE)
-    endif()
+            set(${MODEL} "Raspberry Pi 3 or 4" PARENT_SCOPE)
+		else()
+			message(WARNING "The board revision of this hardware is not known. Please add detection to this board in CMakeLists.txt. (proceeding to compile against a generic multicore CPU)")
+			set(${IS_RPI} 0 PARENT_SCOPE)
+		endif()
+
 endfunction()
